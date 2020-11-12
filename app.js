6df99325a437a68c415f3aaf6fa9ae4d4176e2de
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-11-12 18:15:37 +0100

    quick implement authentication

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const expressValidator = require('express-validator')
 require('dotenv').config()
 
+//import routes
+const authRoutes = require('./routes/auth')
+
+//app
 const app = express()
 
 //db
@@ -17,6 +26,16 @@ mongoose
 mongoose.connection.on('error', (err) => {
   console.log(`DB connection error: ${err.message}`)
 })
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
+app.use(cors())
+
+//routes middleware
+app.use('/api', authRoutes)
 
 const port = process.env.PORT || 8000
 
