e7146e0ea223a5025c66c4970496b2ddb3093605
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-11-12 17:33:56 +0100

    connection DB

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
+
+//db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', (err) => {
+  console.log(`DB connection error: ${err.message}`)
+})
 
 const port = process.env.PORT || 8000
 
