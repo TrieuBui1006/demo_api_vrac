23f8dc3cb0b52928c7843cb123867e45702fc662
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-11-13 21:15:11 +0100

    implement category and user routes

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config()
 
 //import routes
 const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
 
 //app
 const app = express()
@@ -36,6 +38,8 @@ app.use(cors())
 
 //routes middleware
 app.use('/api', authRoutes)
+app.use('/api', userRoutes)
+app.use('/api', categoryRoutes)
 
 const port = process.env.PORT || 8000
 
--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,7 @@ exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
     if (err || !user) {
       return res.status(400).json({
-        err: 'User not found',
+        error: 'User not found',
       })
     }
 
