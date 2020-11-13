a4849a4e9a8169e4f4b31af065919c150fbe3800
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-11-13 20:35:56 +0100

    fix errorhandler

--- helpers/dbErrorHandler.js
+++ helpers/dbErrorHandler.js
@@ -6,12 +6,7 @@
 const uniqueMessage = (error) => {
   let output
   try {
-    let fieldName = error.message.substring(
-      error.message.lastIndexOf('.$') + 2,
-      error.message.lastIndexOf('_1')
-    )
-    output =
-      fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + ' already exists'
+    output = 'Email already exists'
   } catch (ex) {
     output = 'Unique field already exists'
   }
