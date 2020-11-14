28944f7ecc13a0ef8476956b1c69b529a642ea5f
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-11-14 20:34:12 +0100

    add product model

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxlength: 500,
+    },
+    exchangeRatio: {
+      type: Number,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    category: {
+      type: ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Product', productSchema)
