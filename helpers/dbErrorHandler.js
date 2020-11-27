cd9a75ab6cc025ed392803cd05295bbf1a4910d4
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-11-27 21:36:48 +0100

    reform dbErrorHandle

--- helpers/dbErrorHandler.js
+++ helpers/dbErrorHandler.js
@@ -6,7 +6,12 @@
 const uniqueMessage = (error) => {
   let output
   try {
-    output = 'Email already exists'
+    let fieldName = error.message.substring(
+      error.message.lastIndexOf('.$') + 2,
+      error.message.lastIndexOf('_1')
+    )
+    output =
+      fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + ' already exists'
   } catch (ex) {
     output = 'Unique field already exists'
   }
